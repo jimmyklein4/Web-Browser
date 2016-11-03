2ae945d9830080edc87697cce8583fce095c1345
Jimmy Klein <james.klein@temple.edu>
2016-11-03 15:51:56 -0400

    initial

---
+++ app/src/main/java/edu/temple/tuf21842/webbrowser/MainActivity.java
@@ -0,0 +1,22 @@
+package edu.temple.tuf21842.webbrowser;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toolbar;
+
+public class MainActivity extends Activity {
+    private WebPage webPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setActionBar((Toolbar)findViewById(R.id.tools));
+        webPage = new WebPage();
+        getFragmentManager()
+                .beginTransaction()
+                .add(R.layout.fragment_web_page, webPage)
+    }
+}
---
+++ app/src/main/java/edu/temple/tuf21842/webbrowser/WebPage.java
@@ -0,0 +1,44 @@
+package edu.temple.tuf21842.webbrowser;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class WebPage extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+    private final String TAG = "WebPage";
+    public WebPage() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            WebView web = (WebView) getView().findViewById(R.id.web);
+            web.setWebViewClient(new WebViewClient());
+            web.loadUrl("https://google.com/");
+        } catch(NullPointerException e){
+            Log.d(TAG, e.toString());
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_web_page, container, false);
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+
+    }
+}
