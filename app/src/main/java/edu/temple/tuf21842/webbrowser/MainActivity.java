d00bcdf609b0b78b7221d9d7846b0dd9ac24c1f7
Jimmy Klein <jimmy.klein4@gmail.com>
2016-11-13 18:53:48 -0500

    URL bar now loads a different webpage

--- app/src/main/java/edu/temple/tuf21842/webbrowser/MainActivity.java
+++ app/src/main/java/edu/temple/tuf21842/webbrowser/MainActivity.java
@@ -2,16 +2,23 @@ package edu.temple.tuf21842.webbrowser;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toolbar;
 
-public class MainActivity extends Activity {
-    private WebPage webPage;
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends Activity implements WebPage.WebpageListener{
+    private WebPage webPage;
+    private ArrayList<WebPage> allPages;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        allPages = new ArrayList<>();
         setContentView(R.layout.activity_main);
         setActionBar((Toolbar)findViewById(R.id.tools));
         webPage = new WebPage();
@@ -19,5 +26,40 @@ public class MainActivity extends Activity {
                 .beginTransaction()
                 .add(R.id.web_frame, webPage)
                 .commit();
+
+        allPages.add(webPage);
+
+        Button prevButton = (Button) findViewById(R.id.prev_page);
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(allPages.size() > 1){
+                    //TODO go back to prev webpage in list
+                }
+            }
+        });
+
+        Button nextButton = (Button) findViewById(R.id.next_page);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(allPages.size() > 1){
+                    //TODO go to the next page in the list
+                }
+            }
+        });
+
+        ImageButton newPageButton = (ImageButton) findViewById(R.id.new_page);
+        newPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO add a new page to the list
+            }
+        });
+    }
+
+    @Override
+    public void goToNewPage(String URL) {
+
     }
 }
--- app/src/main/java/edu/temple/tuf21842/webbrowser/WebPage.java
+++ app/src/main/java/edu/temple/tuf21842/webbrowser/WebPage.java
@@ -1,48 +1,58 @@
 package edu.temple.tuf21842.webbrowser;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class WebPage extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
-    private final String TAG = "WebPage";
+    private WebpageListener mListener;
     private View v;
+    private final String TAG = "WebpageFragment";
     public WebPage() {
         // Required empty public constructor
-    }
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        try {
-
-        } catch(NullPointerException e){
-            Log.d(TAG, e.toString());
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         v =inflater.inflate(R.layout.fragment_web_page, container, false);
-        WebView web = (WebView) v.findViewById(R.id.web);
+        final WebView web = (WebView) v.findViewById(R.id.web);
         web.setWebViewClient(new WebViewClient());
+        web.getSettings().setJavaScriptEnabled(true);
         web.loadUrl("https://google.com/");
+
+        Button goButton = (Button) v.findViewById(R.id.go_button);
+        final EditText URLbar = (EditText) v.findViewById(R.id.URL_bar);
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newURL = URLbar.getText().toString();
+                if(!newURL.startsWith("http")){
+                    newURL = "http://" + newURL;
+                }
+                web.loadUrl(newURL);
+                InputMethodManager inputManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                inputManager.hideSoftInputFromWindow(v.getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
+            }
+        });
 
         return v;
     }
 
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-
+    public interface WebpageListener {
+        void goToNewPage(String URL);
     }
 }
